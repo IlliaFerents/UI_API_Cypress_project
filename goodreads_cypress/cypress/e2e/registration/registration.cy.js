c7495efd98ffb77071f1f2b385f7d14092c3e05a
Illia Ferents <illiaferents@gmail.com>
2023-12-18 23:05:24 +0200

    create basic registration scenario

---
+++ goodreads_cypress/cypress/e2e/registration/registration.cy.js
@@ -0,0 +1,35 @@
+import RegistrationPage from "../../pages/registrationPage";
+import * as emailUtils from "../../support/utils/emailUtils";
+const { faker } = require("@faker-js/faker");
+
+const registrationPage = new RegistrationPage();
+
+describe("Registration", () => {
+  const fakeName = faker.person.fullName();
+  const password = faker.internet.password();
+  let createdEmail;
+
+  before(() => {
+    emailUtils.createFakeEmail().then((email) => {
+      createdEmail = email;
+    });
+  });
+
+  it("reqisters a new user via emailUtils", () => {
+    cy.visit("/");
+    cy.contains("Sign up with email").click();
+
+    registrationPage.fillNameInput(fakeName);
+    registrationPage.fillEmailInput(createdEmail);
+    registrationPage.fillPasswordInput(password);
+    registrationPage.fillConfirmPasswordInput(password);
+    registrationPage.clickCreateAccount();
+
+    emailUtils.retrieveVerificationCode().then((otpCode) => {
+      registrationPage.fillOtpCodeInput(otpCode);
+      registrationPage.clickSubmitOtpButton();
+    });
+
+    cy.get(".headerTitle").should("contain.text", "Getting Started");
+  });
+});
