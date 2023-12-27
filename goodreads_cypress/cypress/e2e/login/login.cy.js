0d1727d1c7b1713f0f72429c184ba9ebabad9b79
Illia Ferents <illiaferents@gmail.com>
2023-12-27 22:49:06 +0200

    add negative login verification scenario

--- goodreads_cypress/cypress/e2e/login/login.cy.js
+++ goodreads_cypress/cypress/e2e/login/login.cy.js
@@ -10,9 +10,24 @@ describe("Login", () => {
     cy.visit("/");
     cy.contains("Sign In").click();
     cy.contains("Sign in with email").click();
-
-    loginPage.fillEmailInput(userEmail);
-    loginPage.fillPasswordInput(userPassword);
+  it("logs into app with existing user", function () {
+    loginPage.fillEmailInput(this.userData.email);
+    loginPage.fillPasswordInput(this.userData.password);
     loginPage.clickSignInButton();
+
+    cy.contains("Profile").click({ force: true });
+    cy.get("#profileNameTopHeading").should("contain", this.userData.userName);
+  });
+
+  it("displays error message when submitting empty forms", () => {
+    loginPage.clickSignInButton();
+
+    cy.get("#auth-email-missing-alert")
+      .should("be.visible")
+      .and("contain", "Enter your email");
+
+    cy.get("#auth-password-missing-alert")
+      .should("be.visible")
+      .and("contain", "Enter your password");
   });
 });
