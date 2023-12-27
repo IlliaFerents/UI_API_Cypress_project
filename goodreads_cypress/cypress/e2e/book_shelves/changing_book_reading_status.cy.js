9bb1322e70fefb85df355036039fc7e6df2c9641
Illia Ferents <illiaferents@gmail.com>
2023-12-27 22:45:17 +0200

    refactor to optimize runtime performance

--- goodreads_cypress/cypress/e2e/book_shelves/changing_book_reading_status.cy.js
+++ goodreads_cypress/cypress/e2e/book_shelves/changing_book_reading_status.cy.js
@@ -4,23 +4,35 @@ import * as bookActions from "../../support/utils/bookInteractions";
 const myBooksPage = new MyBooks();
 
 describe("Book Reading Status Changes", () => {
-  let bookTitle;
-
-  before(() => {
-    cy.getRandomBookTitle().then((randomBookTitle) => {
-      bookTitle = randomBookTitle;
-
+  before(function () {
+    cy.getRandomBookTitle().then(function (randomBook) {
       cy.login();
       bookActions.removeAllBooksFromAllShelves();
-      bookActions.searchAndAddToReadList(bookTitle);
+      bookActions.searchAndAddToReadList(randomBook);
     });
   });
 
-  it("changes the reading status of a book", () => {
-    cy.visit("/review/list/172896678?ref=nav_mybooks");
+  beforeEach(function () {
+    cy.login();
+    cy.visit("/");
 
+    cy.contains("My Books").click({ force: true });
+  });
+
+  it("verifies that default reading status of a book is Want to Read", () => {
+    myBooksPage.clickEditBookReadStatusButton();
+    myBooksPage.verifyBookReadStatusChosen("to-read");
+  });
+
+  it("changes the reading status of a book to Read", () => {
     myBooksPage.clickEditBookReadStatusButton();
     myBooksPage.selectBookReadStatus("read");
     myBooksPage.verifyBookReadStatusChosen("read");
+  });
+
+  it("changes the reading status of a book to Currently Reading", () => {
+    myBooksPage.clickEditBookReadStatusButton();
+    myBooksPage.selectBookReadStatus("currently-reading");
+    myBooksPage.verifyBookReadStatusChosen("currently-reading");
   });
 });
